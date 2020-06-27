100996ae3847a54b6ab50acfa19fc046e13b0bc0
catemgn <caterina.mogno@gmail.com>
2020-06-27 12:51:29 +0100

    add utils funcs

---
+++ utils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Utils functions for handling WRF-Chem model data. Specific for my PhD project.
+Created on Sat  27 Jun 2020
+
+@author: Caterina Mogno c.mogno@ed.ac.uk
+"""
+import xarray as xr
+import pandas as pd
+
+def reset_time_dim(ds):
+    """
+    Time dimension in WRF-Chem datasets is just the time index [0,1,2,3...].
+    This function convert dimension Time as datetime[64].
+    """
+    return ds.assign_coords(Time=pd.to_datetime(ds.XTIME.values))
+
+
+
+def dictdata(pth_dict):
+    """
+    Return a dictionary of data_name:datasets given a dictionary of 
+    data_name:data_path.
+    Useful format for performing same operations on each dataset.
+  
+    :param pth_dict:
+     dict of paths to datasets.
+    :type pth_dict: dict
+    :return:
+    dictionary of xarray.Dataset.
+    :rtype: dict
+    """
+    
+    d={}
+    for k,s in pth_dict.items():
+        d[k]=xr.open_mfdataset(s)
+        d[k]=reset_time_dim((d[k]))
+        
+    return d
